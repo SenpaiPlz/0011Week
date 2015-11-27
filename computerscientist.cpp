2e948f17f6b349b4b5607a462b017840e343cc82
SenpaiPlz <senpai@plznotice.me>
2015-11-27 11:52:02 +0000

    ComputerScientistClass

---
+++ computerscientist.cpp
@@ -0,0 +1,10 @@
+#include "computerscientist.h"
+
+ComputerScientist::ComputerScientist()
+{
+    name = "";
+    bday = 0;
+    dday = 0;
+    gender = 0;
+}
+
---
+++ computerscientist.h
@@ -0,0 +1,15 @@
+#ifndef COMPUTERSCIENTIST_H
+#define COMPUTERSCIENTIST_H
+#include <string>
+
+class ComputerScientist
+{
+    std::string name;
+    int bday;
+    int dday;
+    bool gender;
+public:
+    ComputerScientist();
+};
+
+#endif // COMPUTERSCIENTIST_H
