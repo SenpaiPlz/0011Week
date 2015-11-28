2b21630e5c67f0f38d8d5a8f2c4ff3a632ba51d6
SenpaiPlz <senpai@plznotice.me>
2015-11-28 01:39:15 +0000

    Added functionality to ComputerScientist

--- computerscientist.cpp
+++ computerscientist.cpp
@@ -1,10 +1,74 @@
 #include "computerscientist.h"
 
+using namespace std;
+//---------------  Construction of Pylons  ---------------//
 ComputerScientist::ComputerScientist()
 {
     name = "";
     bday = 0;
-    dday = 0;
-    gender = 0;
+    dday = -1;
+    gender = "";
 }
 
+ComputerScientist::ComputerScientist(const string& tmpname, const int& tmpbday, const int& tmpdday, const string& tmpgender)
+{
+    name = tmpname;
+    bday = tmpbday;
+    dday = tmpdday;
+    gender = tmpgender;
+}
+
+//---------------  Do we get Set bonuses?  ---------------//
+void ComputerScientist::setname(const string& tmp)
+{
+    name = tmp;
+}
+
+void ComputerScientist::setbday(const int& tmp)
+{
+    bday = tmp;
+}
+
+void ComputerScientist::setdday(const int& tmp)
+{
+    dday = tmp;
+}
+
+void ComputerScientist::setgender(const string& tmp)
+{
+    gender = tmp;
+}
+
+//---------------  Quads Get  ---------------//
+string ComputerScientist::getName()
+{
+    return name;
+}
+string ComputerScientist::getGender()
+{
+    return gender;
+}
+int ComputerScientist::getBday()
+{
+    return bday;
+}
+int ComputerScientist::getDday()
+{
+    return dday;
+}
+
+//---------------  Pedo Mellon a Minno  ---------------//
+
+/*
+ *  Not Sure if we should implement these here...
+ *
+friend istream& operator >>(istream& in, ComputerScientist& cs1)
+{
+
+}
+
+friend ostream& operator <<(ostream& out, ComputerScientist& cs1)
+{
+
+}
+*/
--- computerscientist.h
+++ computerscientist.h
@@ -2,14 +2,33 @@
 #define COMPUTERSCIENTIST_H
 #include <string>
 
+using namespace std;
+
 class ComputerScientist
 {
-    std::string name;
+    string mame;
     int bday;
     int dday;
-    bool gender;
+    string gender;
 public:
     ComputerScientist();
+    ComputerScientist(const string& tmpname, const int& tmpbday, const int& tmpdday, const string& tmpgender);
+
+    string getName();
+    string getGender();
+    int getBday();
+    int getDday();
+
+    void setname(const string& tmp);
+    void setbday(const int& tmp);
+    void setdday(const int& tmp);
+    void setgender(const string& tmp);
+    /*
+     *  Mabey implement somewhere else.
+     *
+    friend istream& operator >>(istream& in, ComputerScientist& cs1);
+    friend ostream& operator <<(ostream& out, ComputerScientist& cs1);
+    */
 };
 
 #endif // COMPUTERSCIENTIST_H
