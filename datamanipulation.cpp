db8cd6c2ef8d387e7edaee08c6a642c3f30c0da5
sigrung15 <sigrung15@ru.is>
2015-11-30 18:33:37 +0000

    updated datamanip file

--- datamanipulation.cpp
+++ datamanipulation.cpp
@@ -1,7 +1,64 @@
 #include "datamanipulation.h"
+#include <algorithm>            //for sort
+#include <vector>
+#include <string>
+
+using namespace std;
 
 DataManipulation::DataManipulation()
 {
+    name = "NA";
+    bday = "NA";
+    dday = "NA";
+    gender = "NA";
+}
 
+bool DataManipulation::sortByName(const DataManipulation& lhs, const DataManipulation& rhs)
+{
+    return lhs.name < rhs.name;
+}
+
+bool DataManipulation::sortByGender(const DataManipulation& lhs, const DataManipulation& rhs)
+{
+    return lhs.gender < rhs.gender;
+}
+
+bool DataManipulation::sortByBday(const DataManipulation& lhs, const DataManipulation& rhs)
+{
+    return lhs.bday < rhs.bday;
+}
+
+bool DataManipulation::sortByDday(const DataManipulation& lhs, const DataManipulation& rhs)
+{
+    return lhs.dday < rhs.dday;
+}
+
+void DataManipulation::print()
+{
+    for(unsigned int i = 0; i != css.size(); i++)
+    {
+        cout << css.at(i).name << " " << css.at(i).gender << " " <<
+                css.at(i).bday << " " << css.at(i).dday << endl;
+    }
+}
+
+void DataManipulation::sortChoice(string& choice)
+{
+    if(choice == name)
+    {
+        sort(css.begin(), css.end(), sortByName);
+    }
+    if(choice == gender)
+    {
+        sort(css.begin(), css.end(), sortByGender);
+    }
+    if(choice == bday)
+    {
+        sort(css.begin(), css.end(), sortByBday);
+    }
+    if(choice == dday)
+    {
+        sort(css.begin(), css.end(), sortByDday);
+    }
 }
 
--- datamanipulation.h
+++ datamanipulation.h
@@ -1,11 +1,26 @@
 #ifndef DATAMANIPULATION_H
 #define DATAMANIPULATION_H
+#include "computerscientist.h"
+#include "databank.h"
+#include "ui.h"
+#include <vector>
+#include <string>
 
 
 class DataManipulation
 {
+    string name;
+    string gender;
+    string bday;
+    string dday;
+    vector<DataManipulation> css;
 public:
     DataManipulation();
+    bool sortByName(const DataManipulation& lhs, const DataManipulation& rhs);
+    bool sortByGender(const DataManipulation& lhs, const DataManipulation& rhs);
+    bool sortByBday(const DataManipulation& lhs, const DataManipulation& rhs);
+    bool sortByDday(const DataManipulation& lhs, const DataManipulation& rhs);
+    void print();
+    void sortChoice(string& choice);
 };
-
 #endif // DATAMANIPULATION_H
