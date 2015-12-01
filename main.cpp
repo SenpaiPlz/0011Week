1039bd68434a6883411e089c047fced3c0848168
sigrung15 <sigrung15@ru.is>
2015-12-01 13:57:04 +0000

    logo added to main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
-//#include <QCoreApplication>
+#include <QCoreApplication>
 #include <iostream>
 #include "computerscientist.h"
 #include "databank.h"
+#include "datamanipulation.h"
 
-int main()//int argc, char *argv[])
+void logo();
+
+int main()//(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
-    DataBank d1;
-    return 0;//a.exec();
+
+    void logo();
+
+    //a.exec();
+    return 0;
 }
 
+void logo()
+{
+    cout << " ________________________________________________\n"
+             << "|                                                |\n"
+             << "|\t _____ _____                 ______\t |\n"
+             << "|\t|  __ |  __ \\               |___  /\t |\n"
+             << "|\t| |  \\| |  \\/  ______    ___   / / \t |\n"
+             << "|\t| | __| | __  |______|  / _ \\ / /  \t |\n"
+             << "|\t| |_\\ | |_\\ \\          |  __./ /___\t |\n"
+             << "|\t \\____/\\____/           \\___\\_____/\t |\n"
+             << "|                                                |\n"
+             << "|________________________________________________|\n";
+}
