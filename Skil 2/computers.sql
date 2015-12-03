87ad90de6dc5a0985d3f113725576e9598cda744
sigrung15 <sigrung15@ru.is>
2015-12-03 15:15:50 +0000

    computer og scientist tables

---
+++ Skil 2/computers.sql
@@ -0,0 +1,7 @@
+INSERT INTO "computers" VALUES(1,'Pascaline',1642,'Mechanical',1,0);
+INSERT INTO "computers" VALUES(2,'ABC',1942,'Electronic',1,0);
+INSERT INTO "computers" VALUES(3,'Colossus',1943,'Electronic',1,0);
+INSERT INTO "computers" VALUES(4,'Difference Engine',1822,'Mechanical',1,0);
+INSERT INTO "computers" VALUES(5,'Analytical Engine',1837,'Mechanical',1,0);
+INSERT INTO "computers" VALUES(6,'Apple 1',1976,'Electronic',1,0);
+INSERT INTO "computers" VALUES(7,'ENIAC',1946,'Electronic',1,0);
