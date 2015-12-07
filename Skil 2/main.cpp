e6ec347a1ef6ec6c596c38228dc222472f5ce1da
SenpaiPlz <senpai@plznotice.me>
2015-12-07 17:25:11 +0000

    My main.cpp

--- Skil 2/main.cpp
+++ Skil 2/main.cpp
@@ -1,44 +1,8 @@
-#include <iostream>
-#include "sqlquerydata.h"
-#include "computerscientist.h"
-#include <vector>
-#include "computersabstract.h"
-#include <QString>
+#include "ui.h"
 using namespace std;
 
 int main()
 {
-
-    SQLQueryData data;
-    data.InitConnect();
-
-    ComputerScientist tere;
-    tere.setFirst("test");
-    tere.setLast("desu");
-    tere.setgender("male");
-    tere.setbday(1999);
-
-    if(data.AddComputerScientist(tere))
-    {
-        cout << "Added to Database";
-    }
-/*
-    vector<ComputerScientist> test = data.GetComputerScientist("first_name",true);
-    for(size_t i = 0; i < test.size(); i++)
-    {
-        cout << i+1 << "\t" << test[i].getID() << ": " << test[i].getFirst() << " ";
-        if(test[i].getMid() != "")
-        {
-        cout << test[i].getMid() << " ";
-        }
-        cout << test[i].getLast() << "\n";
-    }
-    cout << "\n\n";
-    vector<computersabstract> temp = data.GetComputers("name",false);
-    for(size_t i = 0; i < temp.size(); i++)
-    {
-        cout << i+1 << "\t" << temp[i].getID() << ": " << temp[i].getName() << "\n";
-    }
-    */
+    UI init;
     return 0;
 }
