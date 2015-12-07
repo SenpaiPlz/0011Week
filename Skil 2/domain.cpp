fec3977f74f52c5c60afcab7ac0d6664bfb8579d
SenpaiPlz <senpai@plznotice.me>
2015-12-07 15:13:28 +0000

    Added domain class still work in progress

---
+++ Skil 2/domain.cpp
@@ -0,0 +1,24 @@
+#include "domain.h"
+
+domain::domain()
+{
+}
+
+void domain::connect()
+{
+    SQLQueryData init;
+    init.InitConnect();
+}
+
+
+vector<computersabstract> domain::GetComputers(const QString &str, bool desc)
+{
+    SQLQueryData get;
+    return get.GetComputers(str,desc);
+}
+
+vector<ComputerScientist> domain::GetComputerScientist(const QString &str, bool desc)
+{
+    SQLQueryData get;
+    return get.GetComputerScientist(str,desc);
+}
---
+++ Skil 2/domain.h
@@ -0,0 +1,36 @@
+#ifndef DOMAIN_H
+#define DOMAIN_H
+#include <QString>
+#include <vector>
+#include "sqlconnect.h"
+#include <string>
+#include "computerscientist.h"
+#include "computersabstract.h"
+#include "sqlquerydata.h"
+
+class domain
+{
+public:
+    domain();
+    void connect();
+    vector<computersabstract> GetComputers(const QString &str, bool desc);
+    vector<ComputerScientist> GetComputerScientist(const QString &str, bool desc);
+    /*bool AddComputerScientist(ComputerScientist& input);
+    bool AddComputer(computersabstract& input);
+    bool MarkDeleted(const QString &tab, const int& id);
+    bool UnmarkDeleted(const QString& tab, const int& id);
+    bool DeleteAllMarked();
+    vector<ComputerScientist> GetDeletedCS();
+    vector<computersabstract> GetDeletedComputers();
+    vector<int> GetLinkID();
+    bool DeleteLink(const int& rowid);
+    bool AddLink(const int& scientists_id, const int& computers_id);
+    vector<ComputerScientist> SearchCS(const QString& search);
+    vector<ComputerScientist> SearchCSID(const int& search);
+    vector<computersabstract> SearchComputerID(const int& search);
+    vector<computersabstract> SearchComputer(const QString& search);
+    bool UpdateCS(const QString& tempfirst, const QString& tempmid, const QString& templast, const QString& tempgender, const int& bday, const int& dday, const int& id);
+    bool UpdateComputer(const QString &tempname, const int &year, const QString &type, const int &id);*/
+};
+
+#endif // DOMAIN_H
